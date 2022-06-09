9e671df9aa239fd9447a4d54bf6b7913e747d94a
fehnelm <105747696+fehnelm@users.noreply.github.com>
2022-06-09 08:50:44 -0500

    Add files via upload

---
+++ Security Groups.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+
+
+
+.DESCRIPTION
+Pulls all Security Groups within your Forest and Exports the following details to a CSV: Name, CN, Description, Number of Members, and Group Scope.
+
+.NOTES
+Author - Mikki Fehnel
+
+Script Version: 1.0
+#>
+
+$SecurityGroups = Get-ADGroup -Filter 'GroupCategory -eq "Security"'-SearchBase 'DC=Adatum,DC=com'`
+-Properties Name,Members,GroupCategory | Sort -Property Name
+$count = 0
+$groupCount = $ResearchGroups.Count
+
+Write-Host -ForegroundColor Green "Gathering Data on Groups..."
+
+$output = Foreach($group in $SecurityGroups)
+    {
+
+    $numberOfMember = (Get-ADGroup -Identity $group.DistinguishedName -Properties Members).members.count
+    $groupDetails =[PSCustomObject]@{
+    Name = $group.Name
+    CN = $group.DistinguishedName
+    Description = $group.Description
+    "Number of Members" = $numberOfMember
+    "Group Scope"= $group.GroupScope
+            }
+    $count++
+  
+$groupDetails|Export-Csv -Path C:\Temp\SecurityGroup.csv -NoTypeInformation -Append    
+    }
